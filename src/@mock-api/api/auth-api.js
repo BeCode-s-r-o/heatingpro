fb54b0a2d72ff6f3e1b0cbf3e01d77d130d62d83
VladimirTomko <vladimir.tomko@onesimplicity.com>
2022-12-25 18:36:57 +0100

    HP-6: Auth pages design

--- src/@mock-api/api/auth-api.js
+++ src/@mock-api/api/auth-api.js
@@ -21,14 +21,14 @@ mock.onGet('/api/auth/sign-in').reply(async (config) => {
   if (!user) {
     error.push({
       type: 'email',
-      message: 'Check your email address',
+      message: 'Nesprávný email alebo heslo',
     });
   }
 
   if (user && user.password !== password) {
     error.push({
       type: 'password',
-      message: 'Check your password',
+      message: 'Nesprávný email alebo heslo',
     });
   }
 
