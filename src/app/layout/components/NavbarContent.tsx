985df1e80cb6df64fda304ed3e06ecf1d31c0aa5
VladimirTomko <vladimir.tomko@onesimplicity.com>
2022-12-30 03:11:04 +0100

    HP-18: Add build info

--- src/app/layout/components/NavbarContent.tsx
+++ src/app/layout/components/NavbarContent.tsx
@@ -6,6 +6,9 @@ import Logo from '../shared-components/Logo';
 import NavbarToggleButton from '../shared-components/NavbarToggleButton';
 import UserNavbarHeader from '../shared-components/UserNavbarHeader';
 import Navigation from '../shared-components/Navigation';
+//@ts-ignore
+import packagejson from '../../../../package.json';
+import { Typography } from '@mui/material';
 
 const Root = styled('div')(({ theme }) => ({
   backgroundColor: theme.palette.background.default,
@@ -32,7 +35,32 @@ const StyledContent = styled(FuseScrollbars)(({ theme }) => ({
   backgroundAttachment: 'local, scroll',
 }));
 
-function NavbarStyle1Content(props) {
+const NavbarContent = (props) => {
+  const getEnv = () => {
+    if (packagejson.version.includes('alpha')) {
+      return 'alpha';
+    } else if (packagejson.version.includes('beta')) {
+      return 'beta';
+    } else if (packagejson.version.includes('rc')) {
+      return 'rc';
+    } else {
+      return 'stable';
+    }
+  };
+
+  const getBuildDate = () => {
+    if (!packagejson.buildDate) return '';
+    const date = new Date(packagejson.buildDate);
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  };
+
   return (
     <Root className={clsx('flex flex-auto flex-col overflow-hidden h-full', props.className)}>
       <div className="flex flex-row items-center shrink-0 h-48 md:h-72 px-20">
@@ -54,9 +82,12 @@ function NavbarStyle1Content(props) {
         <div className="flex flex-0 items-center justify-center py-48 opacity-10">
           <img className="w-full w-160" src="assets/images/logo/logo.png" alt="footer logo" />
         </div>
+        <Typography textAlign="center" color="rgba(255,255,255,0.3)">
+          v {packagejson.version} {packagejson.buildEnv || 'localhost'} {getBuildDate()}
+        </Typography>
       </StyledContent>
     </Root>
   );
-}
+};
 
-export default memo(NavbarStyle1Content);
+export default memo(NavbarContent);
