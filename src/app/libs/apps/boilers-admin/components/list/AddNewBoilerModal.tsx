d67670d100ac7735b692ae2bfa62a31485e7aa68
Lubomir27 <102256957+Lubomir27@users.noreply.github.com>
2023-02-05 12:45:34 +0100

    added saveNewBoiler function

--- src/app/libs/apps/boilers-admin/components/list/AddNewBoilerModal.tsx
+++ src/app/libs/apps/boilers-admin/components/list/AddNewBoilerModal.tsx
@@ -4,14 +4,19 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import TextField from '@mui/material/TextField';
+import { setDoc, doc } from 'firebase/firestore';
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { db } from 'src/firebase-config';
+import { showMessage } from 'app/store/slices/messageSlice';
 interface Props {
   isOpen: boolean;
   toggleOpen: () => void;
 }
 
 const AddNewBoilerModal = ({ isOpen, toggleOpen }: Props) => {
-  const [newBoiler, setNewBoiler] = useState({ id: '', name: '', period: '', phoneNumber: '' });
+  const dispatch = useDispatch();
+  const [newBoiler, setNewBoiler] = useState({ name: '', phoneNumber: '', assignedTo: '', columns: [] });
 
   const handleChange = (e) => {
     const value = e.target.value;
@@ -21,7 +26,15 @@ const AddNewBoilerModal = ({ isOpen, toggleOpen }: Props) => {
       [e.target.name]: value,
     }));
   };
-
+  const saveNewBoiler = () => {
+    try {
+      const boilerRef = doc(db, 'boilers', '0004A'); //need id?
+      setDoc(boilerRef, newBoiler);
+      dispatch(showMessage({ message: 'Boiler bol úspšene pridaný' }));
+    } catch (error) {
+      dispatch(showMessage({ message: 'Vyskytol sa nejaký problém' }));
+    }
+  };
   return (
     <Drawer anchor="right" open={isOpen} onClose={toggleOpen}>
       <List className="w-[300px]">
@@ -30,25 +43,28 @@ const AddNewBoilerModal = ({ isOpen, toggleOpen }: Props) => {
         </ListItem>
 
         <ListItem>
-          <TextField type="text" label="id" value={newBoiler.id} name="id" onChange={handleChange} />
-        </ListItem>
-        <ListItem>
           <TextField type="text" label="meno" value={newBoiler.name} name="name" onChange={handleChange} />
         </ListItem>
         <ListItem>
-          <TextField type="text" label="perióda" value={newBoiler.period} name="period" onChange={handleChange} />
-        </ListItem>
-        <ListItem>
           <TextField
-            type="number"
-            label="perióda"
+            type="text"
+            label="telefónne číslo"
             value={newBoiler.phoneNumber}
             name="phoneNumber"
             onChange={handleChange}
           />
         </ListItem>
+        <ListItem>
+          <TextField
+            type="text"
+            label="majiteľ"
+            value={newBoiler.assignedTo}
+            name="assignedTo"
+            onChange={handleChange}
+          />
+        </ListItem>
         <ListItem className="flex justify-around">
-          <Button className="whitespace-nowrap" variant="contained" color="secondary" onClick={() => {}}>
+          <Button className="whitespace-nowrap" variant="contained" color="secondary" onClick={() => saveNewBoiler}>
             Uložiť
           </Button>
           <Button className="whitespace-nowrap" variant="contained" color="primary" onClick={toggleOpen}>
