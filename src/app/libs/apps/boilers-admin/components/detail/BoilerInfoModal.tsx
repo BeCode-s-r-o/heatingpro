ffe5da838f6c529816915f3f8a4e64692fecb577
Lubomir27 <102256957+Lubomir27@users.noreply.github.com>
2023-02-17 20:56:58 +0100

    created separated BoilerInfoModal component

--- src/app/libs/apps/boilers-admin/components/detail/BoilerInfoModal.tsx
+++ src/app/libs/apps/boilers-admin/components/detail/BoilerInfoModal.tsx
@@ -1,63 +1,53 @@
-import { useRef, useState } from 'react';
-import { showMessage } from 'app/store/slices/messageSlice';
 import Button from '@mui/material/Button';
 import Drawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
-import ListItemSecondaryAction from '@mui/material/ListItemSecondaryAction';
 import ListItemText from '@mui/material/ListItemText';
-import Switch from '@mui/material/Switch';
 import TextField from '@mui/material/TextField';
-import {
-  doc,
-  updateDoc,
-  getDoc,
-  collection,
-  deleteDoc,
-  setDoc,
-  getFirestore,
-  query,
-  where,
-  getDocs,
-} from 'firebase/firestore';
-import firebase from 'firebase/app';
+import { showMessage } from 'app/store/slices/messageSlice';
 import 'firebase/firestore';
+import { doc, updateDoc } from 'firebase/firestore';
+import { useState } from 'react';
 
-import { tempColumns } from '../../constants';
-import { db } from 'src/firebase-config';
+import { TBoiler, TBoilerInfo } from '@app/types/TBoilers';
+import { AppDispatch } from 'app/store/index';
 import { useDispatch } from 'react-redux';
-import { TBoiler, TBoilerInfo, TSms } from '@app/types/TBoilers';
-import { id } from 'date-fns/locale';
+import { db } from 'src/firebase-config';
+import { getBoiler } from '../../store/boilersSlice';
 interface Props {
   boilerInfo: TBoilerInfo;
+  boilerData: TBoiler;
   isOpen: boolean;
   toggleOpen: () => void;
 }
 
-function BoilerInfoModal({ boilerInfo, isOpen, toggleOpen }: Props) {
-  const [newBoiler, setNewBoiler] = useState(boilerInfo);
-  const dispatch = useDispatch();
+function ChangeHeaderInfoModal({ boilerInfo, boilerData, isOpen, toggleOpen }: Props) {
+  const [headerData, setHeaderData] = useState(boilerInfo);
+  const dispatch = useDispatch<AppDispatch>();
 
   const handleChange = (e) => {
     const value = e.target.value;
-    setNewBoiler((prev) => ({
+    setHeaderData((prev) => ({
       ...prev,
       [e.target.name]: value,
     }));
   };
 
-  const setNewBoilerDocument = (id, data) => {
+  const uptadeHeaderInfo = (id, data) => {
     const newBoilerRef = doc(db, 'boilers', id);
-    setDoc(newBoilerRef, data);
+    updateDoc(newBoilerRef, data);
   };
 
   const updateBoilerDocument = () => {
     try {
-      setNewBoilerDocument(newBoiler.monitoringDeviceID, newBoiler);
+      const data = { ...boilerData, header: headerData };
+      uptadeHeaderInfo(boilerData.id, data);
+      dispatch(getBoiler(boilerData.id || ''));
       dispatch(showMessage({ message: 'Zmeny boli uložené' }));
       toggleOpen();
     } catch (error) {
       toggleOpen();
+      console.log(error);
       dispatch(showMessage({ message: 'Vyskytol sa nejaký problém' }));
     }
   };
@@ -66,27 +56,75 @@ function BoilerInfoModal({ boilerInfo, isOpen, toggleOpen }: Props) {
     <Drawer anchor="right" open={isOpen} onClose={toggleOpen}>
       <List className="w-[300px]">
         <ListItem>
-          <ListItemText primary="Nastavenie Parametrov" />
-        </ListItem>
-
-        <ListItem>
-          <TextField type="text" label="id" value={newBoiler.name} name="id" onChange={handleChange} />
-        </ListItem>
-        <ListItem>
-          <TextField type="text" label="meno" value={newBoiler.location} name="name" onChange={handleChange} />
-        </ListItem>
-        <ListItem>
-          <TextField type="text" label="perióda" value={newBoiler.provider} name="period" onChange={handleChange} />
-        </ListItem>
-        <ListItem>
-          <TextField type="text" label="perióda" value={newBoiler.maintenance} name="period" onChange={handleChange} />
+          <ListItemText primary="Nastavenie Info" />
         </ListItem>
         <ListItem>
           <TextField
+            className="w-full"
             type="text"
-            label="perióda"
-            value={newBoiler.monitoringDeviceID}
-            name="period"
+            label="Názov"
+            value={headerData.name}
+            name="name"
+            onChange={handleChange}
+          />
+        </ListItem>
+        <ListItem>
+          <TextField
+            className="w-full"
+            type="text"
+            label="Umiestnenie"
+            value={headerData.location}
+            name="location"
+            onChange={handleChange}
+          />
+        </ListItem>
+        <ListItem>
+          <TextField
+            className="w-full"
+            type="text"
+            label="Prevádzkovateľ"
+            value={headerData.provider}
+            name="provider"
+            onChange={handleChange}
+          />
+        </ListItem>
+        <ListItem>
+          <TextField
+            className="w-full"
+            type="text"
+            label="Obsluha"
+            value={headerData.maintenance}
+            name="maintenance"
+            onChange={handleChange}
+          />
+        </ListItem>
+        <ListItem>
+          <TextField
+            className="w-full"
+            type="text"
+            label="Kúrič 1"
+            value={headerData.staff1}
+            name="staff1"
+            onChange={handleChange}
+          />
+        </ListItem>
+        <ListItem>
+          <TextField
+            className="w-full"
+            type="text"
+            label="Kúrič 2"
+            value={headerData.staff2}
+            name="staff2"
+            onChange={handleChange}
+          />
+        </ListItem>
+        <ListItem>
+          <TextField
+            className="w-full"
+            type="text"
+            label="ID"
+            value={headerData.monitoringDeviceID}
+            name="monitoringDeviceID"
             onChange={handleChange}
           />
         </ListItem>
@@ -108,4 +146,4 @@ function BoilerInfoModal({ boilerInfo, isOpen, toggleOpen }: Props) {
   );
 }
 
-export default BoilerInfoModal;
+export default ChangeHeaderInfoModal;
--- src/app/libs/apps/boilers-admin/components/detail/BoilersDetail.tsx
+++ src/app/libs/apps/boilers-admin/components/detail/BoilersDetail.tsx
@@ -1,18 +1,15 @@
 import { TBoiler } from '@app/types/TBoilers';
-import FuseSvgIcon from '@app/core/SvgIcon';
 import { AppDispatch, RootState } from 'app/store/index';
 import { selectUser } from 'app/store/userSlice';
 import withReducer from 'app/store/withReducer';
 import { motion as m } from 'framer-motion';
-import Button from '@mui/material/Button';
 import { useEffect } from 'react';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
 import { useDispatch, useSelector } from 'react-redux';
 import { Navigate, useParams } from 'react-router-dom';
 import { container, item } from '../../constants';
 import { boilersSlice, getBoilers, selectBoilerById } from '../../store/boilersSlice';
 import { Wrapper } from '../styled/BoilersStyled';
+import BoilerFooter from './BoilerFooter';
 import { BoilersDetailHeader } from './BoilersDetailHeader';
 import { BoilersDetailTable } from './BoilersDetailTable';
 
@@ -43,38 +40,10 @@ const BoilersDetail = () => {
             animate="show"
           >
             <m.div variants={item} className="sm:col-span-6">
-              <Paper className="flex flex-col flex-auto p-24 shadow rounded-2xl overflow-hidden">
-                <Typography className="text-xl font-light tracking-tight leading-6 truncate">
-                  <strong className="font-semibold">Názov kotolne:</strong> nejaké dáta
-                </Typography>
-                <Typography className="text-xl pt-7 font-light tracking-tight leading-6 truncate">
-                  <strong className="font-semibold">Umiestnenie v objekte:</strong> nejaké dáta
-                </Typography>
-                <Typography className="text-xl pt-7 font-light tracking-tight leading-6 truncate">
-                  <strong className="font-semibold">Prevádzkovateľ:</strong> nejaké dáta
-                </Typography>
-                <Typography className="text-xl pt-7 font-light tracking-tight leading-6 truncate">
-                  <strong className="font-semibold">Obsluha kotolne :</strong> Heating pro s.r.o. - Fero Kurič +421 907
-                  899 523
-                </Typography>
-                <Typography className="text-xl pt-7 font-light tracking-tight leading-6 truncate">
-                  <strong className="font-semibold">Kúrič 1 :</strong>
-                </Typography>
-                <Typography className="text-xl pt-7 font-light tracking-tight leading-6 truncate">
-                  <strong className="font-semibold">ID Monitorovacieho zariadenia:</strong> nejaké dáta
-                </Typography>
-                <Button
-                  className="whitespace-nowrap w-fit mt-16 "
-                  variant="contained"
-                  color="secondary"
-                  startIcon={<FuseSvgIcon size={20}>heroicons-solid:cog</FuseSvgIcon>}
-                >
-                  Zmeniť údaje
-                </Button>
-              </Paper>
+              <BoilersDetailTable id={id} />
             </m.div>
             <m.div variants={item} className="sm:col-span-6">
-              <BoilersDetailTable id={id} />
+              {boiler && <BoilerFooter boiler={boiler} />}
             </m.div>
           </m.div>
         </div>
