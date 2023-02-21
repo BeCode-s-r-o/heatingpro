5755cd5428d49081f0a767c482972170aacc409d
VladimirTomko <vladimir.tomko@onesimplicity.com>
2023-02-21 10:42:38 +0100

    Settings

--- src/app/libs/pages/activities/ActivitiesPage.js
+++ src/app/libs/pages/activities/ActivitiesPage.js
@@ -1,9 +1,6 @@
 import FusePageSimple from '@app/core/PageSimple';
 import useThemeMediaQuery from '@app/hooks/useThemeMediaQuery';
-import Timeline from '@mui/lab/Timeline';
 import Typography from '@mui/material/Typography';
-import ActivityTimelineItem from './ActivityTimelineItem';
-import exampleActivitiesData from './exampleActivitiesData';
 
 function ActivitiesPage() {
   const isMobile = useThemeMediaQuery((theme) => theme.breakpoints.down('lg'));
@@ -12,23 +9,7 @@ function ActivitiesPage() {
     <FusePageSimple
       content={
         <div className="flex flex-col flex-auto px-24 py-40 sm:px-64 sm:pt-72 sm:pb-80">
-          <Typography className="text-4xl font-extrabold tracking-tight leading-none">All Activities</Typography>
-          <Typography className="mt-6 text-lg" color="text.secondary">
-            Application wide activities are listed here as individual items, starting with the most recent.
-          </Typography>
-          <Timeline
-            className="py-48"
-            position="right"
-            sx={{
-              '& .MuiTimelineItem-root:before': {
-                display: 'none',
-              },
-            }}
-          >
-            {exampleActivitiesData.map((item, index) => (
-              <ActivityTimelineItem last={exampleActivitiesData.length === index + 1} item={item} key={item.id} />
-            ))}
-          </Timeline>
+          <Typography className="text-4xl font-extrabold tracking-tight leading-none">Nastavenia</Typography>
         </div>
       }
       scroll={isMobile ? 'normal' : 'page'}
