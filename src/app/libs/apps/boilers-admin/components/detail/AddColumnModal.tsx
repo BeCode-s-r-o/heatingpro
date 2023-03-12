218d8cd5ae7b1dc954ef81e2dd30feaa7ce94c17
Lubomir27 <102256957+Lubomir27@users.noreply.github.com>
2023-03-12 17:45:06 +0100

    AddColumn component

--- src/app/libs/apps/boilers-admin/components/detail/AddColumnModal.tsx
+++ src/app/libs/apps/boilers-admin/components/detail/AddColumnModal.tsx
@@ -1,23 +1,47 @@
 import { Drawer, List, ListItem, ListItemText, TextField, Button } from '@mui/material';
 import { useState } from 'react';
 import FuseSvgIcon from '@app/core/SvgIcon';
-function AddColumnModal({ isOpen, close, columns, deviceID }) {
-  const [formFields, setFormFields] = useState([{ name: '' }]);
+import { useDispatch } from 'react-redux';
+import { showMessage } from 'app/store/slices/messageSlice';
+import { doc, updateDoc } from 'firebase/firestore';
+import { db } from 'src/firebase-config';
+import { getBoiler } from '../../store/boilersSlice';
+import { AppDispatch } from 'app/store/index';
+interface Props {
+  isOpen: boolean;
+  close: () => void;
+  columns: { field: string; headerName: string }[];
+  rows: {}[];
+  deviceID: string;
+}
 
-  const handleFormChange = (event, index) => {
+function AddColumnModal({ isOpen, close, columns, deviceID, rows }: Props) {
+  const [formFields, setFormFields] = useState([{ name: '' }]);
+  const dispatch = useDispatch<AppDispatch>();
+  const hanldeChange = (event, index) => {
+    let { name, value } = event.target;
     let data = [...formFields];
-    data[index][event.target.name] = event.target.value;
+    data[index][name] = value;
     setFormFields(data);
   };
 
   const submit = (e) => {
     e.preventDefault();
+
     const newColumns = formFields.map((column) => ({ field: column.name, headerName: column.name }));
-    if (existDuplicateColumn()) {
-      console.log('Duplicate column');
+    if (existDuplicateColumn([...newColumns, ...columns])) {
+      dispatch(showMessage({ message: 'Vyskytol sa duplicitný stĺpec' }));
       return;
     }
-    console.log(newColumns);
+    try {
+      const boilerRef = doc(db, 'boilers', deviceID);
+      updateDoc(boilerRef, { monthTable: { columns: [...columns, ...newColumns], rows: addEmptyValueForRows() } });
+      close();
+    } catch {
+      dispatch(showMessage({ message: 'Ups, vyskytla sa chyba' }));
+    }
+    dispatch(getBoiler(deviceID || ''));
+    dispatch(showMessage({ message: 'Stĺpec úspešné pridaný' }));
   };
 
   const addFields = () => {
@@ -27,20 +51,32 @@ function AddColumnModal({ isOpen, close, columns, deviceID }) {
 
     setFormFields([...formFields, object]);
   };
+  const addEmptyValueForRows = () => {
+    const newColumns = formFields.map((column) => column.name);
+    const updatedRows: {}[] = [];
+    for (let i = 0; i < rows.length; i++) {
+      var updatedRow = { ...rows[i] };
+      for (let j = 0; j < newColumns.length; j++) {
+        updatedRow[newColumns[j]] = '-';
+        console.log(i, j);
+      }
+      updatedRows.push(updatedRow);
+    }
+    return updatedRows;
+  };
 
   const removeFields = (index) => {
     let data = [...formFields];
     data.splice(index, 1);
     setFormFields(data);
   };
-  const existDuplicateColumn = () => {
+  const existDuplicateColumn = (columns) => {
     const names = new Set();
     for (let column of columns) {
-      //@ts-ignore
-      if (names.has(column.name)) {
+      if (names.has(column.field)) {
         return true;
-      } //@ts-ignore
-      names.add(column.name);
+      }
+      names.add(column.field);
     }
     return false;
   };
@@ -57,7 +93,7 @@ function AddColumnModal({ isOpen, close, columns, deviceID }) {
                 name="name"
                 label="Názov"
                 placeholder="Názov"
-                onChange={(event) => handleFormChange(event, index)}
+                onChange={(event) => hanldeChange(event, index)}
                 value={form.name}
                 required
               />
