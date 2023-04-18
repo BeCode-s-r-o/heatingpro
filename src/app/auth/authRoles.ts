7afb12f2ed210af6c278517a72e4bb183c291fa0
Lubomir27 <102256957+Lubomir27@users.noreply.github.com>
2023-04-18 15:46:47 +0200

    auth roles + (not)fixed columns

--- src/app/auth/authRoles.ts
+++ src/app/auth/authRoles.ts
@@ -5,6 +5,8 @@ export const authRoles = {
   admin: ['admin'],
   instalater: ['admin', 'staff', 'instalater'],
   staff: ['admin', 'staff'],
+  allRoles: ['admin', 'staff', 'instalater', 'user', 'obsluha'],
+  rolesEnabledAddPeople: ['admin', 'instalater', 'user'],
   kuric: [],
   obsluha: [],
   user: ['admin', 'user'],
--- src/app/libs/apps/boilers-admin/components/list/BoilersList.tsx
+++ src/app/libs/apps/boilers-admin/components/list/BoilersList.tsx
@@ -4,7 +4,7 @@ import { AppDispatch } from 'app/store/index';
 import { selectUser } from 'app/store/userSlice';
 import withReducer from 'app/store/withReducer';
 import { motion } from 'framer-motion';
-import { useEffect } from 'react';
+import { memo, useEffect, useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import { container, item } from '../../constants';
@@ -13,36 +13,42 @@ import { Wrapper } from '../styled/BoilersStyled';
 import { BoilersListHeader } from './BoilersListHeader';
 import { BoilersListTable } from './BoilersListTable';
 
+const filterAssignedHeaters = (heaters: TBoiler[], ids: string[]) => {
+  return heaters.filter((heater) => ids.includes(heater.id));
+};
+
+const getAssignedHeatersIds = (heaters: TContactHeater[] | []) => {
+  return heaters.map((device) => device.heater);
+};
+
 const BoilersList = () => {
-  const filterAssignedHeaters = (heaters: TBoiler[], ids: string[]) => {
-    return heaters.filter((heater) => ids.includes(heater.id));
-  };
-  const getAssignedHeatersIds = (heaters: TContactHeater[] | []) => {
-    return heaters.map((device) => device.heater);
-  };
   const dispatch = useDispatch<AppDispatch>();
-  const data = useSelector(selectAllBoilers);
+  const arrayOfAllBoilers = useSelector(selectAllBoilers);
 
   const { data: userData } = useSelector(selectUser);
   const allowedAuthRoles = ['admin', 'staff', 'instalater'];
   const userRole = userData?.role || '';
+  const isAdmin = userRole === 'admin';
+
+  const assignedHeaterIds = useMemo(() => getAssignedHeatersIds(userData?.heaters || []), [userData?.heaters]);
 
   const boilers = () => {
-    if (allowedAuthRoles.includes(userRole)) {
-      return data;
+    if (isAdmin) {
+      return arrayOfAllBoilers;
     } else {
-      let ids = getAssignedHeatersIds(userData?.heaters || []);
-      return filterAssignedHeaters(data, ids);
+      return filterAssignedHeaters(arrayOfAllBoilers, assignedHeaterIds);
     }
   };
 
   useEffect(() => {
-    if (userRole && allowedAuthRoles.includes(userRole)) {
+    if (userData?.role && allowedAuthRoles.includes(userData?.role)) {
       dispatch(getBoilers());
     }
-  }, [dispatch]);
+  }, [dispatch, userData?.role, allowedAuthRoles]);
 
-  return !allowedAuthRoles.includes(userRole) ? (
+  const BoilersListTableMemo = memo(BoilersListTable);
+
+  return !allowedAuthRoles.includes(userData?.role || '') ? (
     <Navigate to="/pouzivatelske-systemy/" replace />
   ) : (
     <Wrapper
