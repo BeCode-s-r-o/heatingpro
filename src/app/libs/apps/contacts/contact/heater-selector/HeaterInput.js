99decca48421d563d5d667318e217e39d6c7db59
Lubomir27 <102256957+Lubomir27@users.noreply.github.com>
2023-04-27 21:30:24 +0200

    heater input autocoplete fixes

--- src/app/libs/apps/contacts/contact/heater-selector/HeaterInput.js
+++ src/app/libs/apps/contacts/contact/heater-selector/HeaterInput.js
@@ -1,15 +1,13 @@
 import FuseSvgIcon from '@app/core/SvgIcon';
 import { yupResolver } from '@hookform/resolvers/yup/dist/yup';
+import Autocomplete from '@mui/material/Autocomplete/Autocomplete';
 import IconButton from '@mui/material/IconButton';
 import TextField from '@mui/material/TextField';
 import { useEffect, useState } from 'react';
-import { Controller, useForm } from 'react-hook-form';
-import * as yup from 'yup';
-import Autocomplete from '@mui/material/Autocomplete/Autocomplete';
-
+import { useForm } from 'react-hook-form';
 import { useDispatch, useSelector } from 'react-redux';
-import { boilersSlice, getBoilers, selectAllBoilers } from '../../../boilers-admin/store/boilersSlice';
-import withReducer from 'app/store/withReducer';
+import * as yup from 'yup';
+import { getBoilers, selectAllBoilers } from '../../../boilers-admin/store/boilersSlice';
 
 const schema = yup.object().shape({
   heater: yup.string().required('Pridajte id kotla'),
@@ -19,9 +17,19 @@ const schema = yup.object().shape({
 
 function HeaterInput(props) {
   const { value, hideRemove } = props;
-  const [actualHeaterInfo, setActualHeaterInfo] = useState({ id: '', name: '', phoneNumber: '' });
   const arrayOfAllBoilers = useSelector(selectAllBoilers);
+  const [actualHeaterInfo, setActualHeaterInfo] = useState({ id: '', name: '', phoneNumber: '' });
+
   const dispatch = useDispatch();
+  useEffect(() => {
+    arrayOfAllBoilers.length > 0 &&
+      setActualHeaterInfo(
+        value === undefined // when you are adding new boiler
+          ? { id: '', name: '', phoneNumber: '' }
+          : arrayOfAllBoilers.find((boiler) => boiler.id === value)
+      );
+  }, [arrayOfAllBoilers]);
+
   useEffect(() => {
     dispatch(getBoilers());
   }, [dispatch]);
@@ -42,52 +50,7 @@ function HeaterInput(props) {
   function onSubmit(data) {
     props.onChange(data.id);
     setActualHeaterInfo(data);
-
   }
-  const heaters = [
-    {
-      id: 'c31e9e5d-e0cb-4574-a13f-8a6ee5ff8309',
-
-      label: 'Piecka',
-      phone: '0907 961 609',
-    },
-    {
-      id: 'a8991c76-2fda-4bbd-a718-df13d6478847',
-
-      label: 'Piecka',
-      phone: '0907 961 609',
-    },
-    {
-      id: '56ddbd47-4078-4ddd-8448-73c5e88d5f59',
-
-      label: 'Piecka',
-      phone: '0907 961 609',
-    },
-    {
-      id: '2026ce08-d08f-4b4f-9506-b10cdb5b104f',
-
-      label: 'Piecka',
-      phone: '0907 961 609',
-    },
-    {
-      id: '65930b5a-5d2a-4303-b11f-865d69e6fdb5',
-
-      label: 'Piecka',
-      phone: '0907 961 609',
-    },
-    {
-      id: '3eaab175-ec0d-4db7-bc3b-efc633c769be',
-
-      label: 'Piecka',
-      phone: '0907 961 609',
-    },
-    {
-      id: 'cbde2486-5033-4e09-838e-e901b108cd41',
-
-      label: 'Piecka',
-      phone: '0907 961 609',
-    },
-  ];
 
   return (
     <>
@@ -95,10 +58,10 @@ function HeaterInput(props) {
       <form className="flex space-x-16 mb-16">
         <Autocomplete
           disablePortal
-          id="id"
           options={arrayOfAllBoilers}
           getOptionLabel={(option) => option.id}
           isOptionEqualToValue={(option, value) => option.id === value.id}
+          value={actualHeaterInfo}
           renderOption={(_props, option) => <li {..._props}>{option.id}</li>}
           onChange={(event, newValue) => {
             onSubmit(newValue);
@@ -138,7 +101,6 @@ function HeaterInput(props) {
             readOnly: true,
           }}
         />
-
         {!hideRemove && (
           <IconButton onClick={props.onRemove}>
             <FuseSvgIcon size={20}>heroicons-solid:trash</FuseSvgIcon>
