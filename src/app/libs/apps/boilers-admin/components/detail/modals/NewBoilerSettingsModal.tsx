b88d86297a3d9b63b2b631474b84e026d022977d
Lubomir27 <102256957+Lubomir27@users.noreply.github.com>
2023-06-24 17:45:51 +0200

    columns select in AddNewBoiler

--- src/app/libs/apps/boilers-admin/components/detail/modals/NewBoilerSettingsModal.tsx
+++ src/app/libs/apps/boilers-admin/components/detail/modals/NewBoilerSettingsModal.tsx
@@ -18,11 +18,44 @@ import { getBoiler } from '../../../store/boilersSlice';
 import { AppDispatch } from 'app/store/index';
 import HeightIcon from '@mui/icons-material/Height';
 import SettingsModalColumn from '../modals/SettingsModalColumn';
+import Select from '@mui/material/Select';
+import FormControl from '@mui/material/FormControl';
+import MenuItem from '@mui/material/MenuItem';
+
 interface Props {
   boiler: TBoiler;
   isOpen: boolean;
   toggleOpen: () => void;
 }
+
+const columnOptions = [
+  { name: 'K1', desc: 'Kotol 1', unit: '0/1' },
+  { name: 'K2', desc: 'Kotol 2', unit: '0/1' },
+  { name: 'K3', desc: 'Kotol 3', unit: '0/1' },
+  { name: 'K4', desc: 'Kotol 4', unit: '0/1' },
+  { name: 'K5', desc: 'Kotol 5', unit: '0/1' },
+  { name: 'VO6', desc: 'Vykurovací okruh prívod 1', unit: '°C' },
+  { name: 'VO7', desc: 'Vykurovací okruh spiatočka 1', unit: '°C' },
+  { name: 'VO8', desc: 'Vykurovací okruh prívod 2', unit: '°C' },
+  { name: 'MT TUV', desc: 'Vykurovací okruh spiatočka 2', unit: '°C' },
+  { name: 'VnDVS', desc: 'Vykurovací okruh prívod 3', unit: '°C' },
+  { name: 'VCH', desc: 'Vykurovací okruh spiatočka 3', unit: '°C' },
+  { name: 'VpZ', desc: 'Vykurovací okruh prívod 4', unit: '°C' },
+  { name: 'MT VZT', desc: 'Vykurovací okruh spiatočka 4', unit: '°C' },
+  { name: 'Elektro', desc: 'Vykurovací okruh prívod 5', unit: '°C' },
+  { name: 'Plyn', desc: 'Vykurovací okruh spiatočka 5', unit: '°C' },
+  { name: 'TPP', desc: 'Teplota prívod primár', unit: '°C' },
+  { name: 'TSP', desc: 'Teplota spiatočka primár', unit: '°C' },
+  { name: 'VT', desc: 'Vonkajšia teplota', unit: '°C' },
+  { name: 'T TUV', desc: 'teplota teplej užitkovej vody', unit: '°C' },
+  { name: 'CO2', desc: 'Snímač CO2', unit: 'ppm' },
+  { name: 'TV', desc: 'Snímač tlaku vykurovania', unit: 'kPa' },
+  { name: 'TP', desc: 'Snímač tlaku plynu', unit: 'kPa' },
+  { name: 'U', desc: 'Snímač zaplavenia kotolne', unit: '0/1' },
+  { name: 'UP', desc: 'Snímač úniku plynu', unit: '0/1' },
+  { name: 'SPK', desc: 'Sumárna porucha kotolne', unit: '0/1' },
+];
+
 function NewBoilerSettingsModal({ boiler, isOpen, toggleOpen }: Props) {
   const [digitalInput, setDigitalInput] = useState<any>([]);
   const [inputData, setInputData] = useState<any>([]);
@@ -76,6 +109,31 @@ function NewBoilerSettingsModal({ boiler, isOpen, toggleOpen }: Props) {
       dispatch(showMessage({ message: 'Vyskytol sa nejaký problém' }));
     }
   };
+
+  const createColumnNameHandleChange = (setState) => (e, field, value) => {
+    if (field === 'hide') {
+      value = !e.target.checked;
+    }
+
+    setState((prev) =>
+      prev.map((column) => {
+        if (column.accessor === e.target.name) {
+          const selectedColumnName = value as string;
+          const selectedColumn = columnOptions.find((column) => column.name === selectedColumnName);
+
+          return {
+            ...column,
+            [field]: value,
+            desc: selectedColumn?.desc || '',
+            unit: selectedColumn?.unit || '',
+          };
+        }
+        return column;
+      })
+    );
+  };
+  const handleDataNameChange = useCallback(createColumnNameHandleChange(setInputData), []);
+  const handleInNameChange = useCallback(createColumnNameHandleChange(setDigitalInput), []);
   return (
     <Drawer anchor="right" open={isOpen} onClose={toggleOpen}>
       <div className="max-w-[98vw] overflow-x-scroll">
@@ -85,12 +143,23 @@ function NewBoilerSettingsModal({ boiler, isOpen, toggleOpen }: Props) {
           </ListItem>
           <Typography className="text-lg font-bold text-center border-b my-12">Dáta zo vstupu</Typography>
           {inputData.map((item, index) => (
-            <SettingsModalColumn key={index} item={item} handleChange={handleDataChange} />
+            <SettingsModalColumn
+              key={index}
+              item={item}
+              handleChange={handleDataChange}
+              columnOptions={columnOptions}
+              handleColumnNameChange={handleDataNameChange}
+            />
           ))}
           <Typography className="text-lg font-bold text-center border-b my-12">Digitálny vstup</Typography>
-
           {digitalInput.map((item, index) => (
-            <SettingsModalColumn key={index} item={item} handleChange={handleInChange} />
+            <SettingsModalColumn
+              key={index}
+              item={item}
+              handleChange={handleInChange}
+              columnOptions={columnOptions}
+              handleColumnNameChange={handleInNameChange}
+            />
           ))}
           <ListItem className="flex justify-end gap-12 sticky bottom-0 z-50 bg-white">
             <Button
--- src/app/libs/apps/boilers-admin/components/detail/modals/SettingsModalColumn.tsx
+++ src/app/libs/apps/boilers-admin/components/detail/modals/SettingsModalColumn.tsx
@@ -1,23 +1,5 @@
 import React, { memo } from 'react';
-import { useRef, useState } from 'react';
-import { showMessage } from 'app/store/slices/messageSlice';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Drawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemSecondaryAction from '@mui/material/ListItemSecondaryAction';
-import ListItemText from '@mui/material/ListItemText';
-import Switch from '@mui/material/Switch';
-import TextField from '@mui/material/TextField';
-import { doc, updateDoc, getDoc } from 'firebase/firestore';
-
-import { db } from 'src/firebase-config';
-import { TBoiler } from '@app/types/TBoilers';
-import { useDispatch } from 'react-redux';
-import { getBoiler } from '../../../store/boilersSlice';
-import { AppDispatch } from 'app/store/index';
-import HeightIcon from '@mui/icons-material/Height';
+import { TextField, ListItem, Typography, Switch, MenuItem } from '@mui/material';
 
 interface SettingsModalColumnProps {
   item: {
@@ -31,62 +13,69 @@ interface SettingsModalColumnProps {
     accessor: string;
   };
   handleChange: (e: any, field: string, value: string | number | boolean) => void;
+  columnOptions: { name: string; desc: string; unit: string }[];
+  handleColumnNameChange: (e: any, field: string, value: string) => void;
 }
 
-const SettingsModalColumn = memo(({ item, handleChange }: SettingsModalColumnProps) => {
-  return (
-    <ListItem>
-      {' '}
-      <Typography className="text-lg font-bold mr-8">{item.value}</Typography>
-      <TextField
-        type="text"
-        label="Názov"
-        value={item.columnName}
-        name={item.accessor}
-        onChange={(e) => handleChange(e, 'columnName', e.target.value)}
-        className="w-[165px] "
-      />
-      <TextField
-        type="text"
-        label="Vysvetlivka"
-        value={item.desc}
-        name={item.accessor}
-        onChange={(e) => handleChange(e, 'desc', e.target.value)}
-        className="w-[255px] "
-      />
-      <TextField
-        type="text"
-        label="Jednotka"
-        value={item.unit}
-        name={item.accessor}
-        onChange={(e) => handleChange(e, 'unit', e.target.value)}
-        className="w-[80px] px-6"
-      />
-      <TextField
-        type="number"
-        label="Min."
-        value={item.min || undefined}
-        name={item.accessor}
-        onChange={(e) => handleChange(e, 'min', Number(e.target.value))}
-        className="w-[70px] pr-6"
-      />
-      <TextField
-        type="number"
-        label="Max."
-        value={item.max || undefined}
-        name={item.accessor}
-        onChange={(e) => handleChange(e, 'max', Number(e.target.value))}
-        className="w-[70px]"
-      />{' '}
-      <Switch
-        checked={!item.hide}
-        name={item.accessor}
-        onChange={(e) => {
-          handleChange(e, 'hide', !e.target.checked);
-        }}
-      />
-    </ListItem>
-  );
-});
+const SettingsModalColumn = memo(
+  ({ item, handleChange, columnOptions, handleColumnNameChange }: SettingsModalColumnProps) => {
+    return (
+      <ListItem>
+        <Typography className="text-lg font-bold mr-8">{item.value}</Typography>
+        <TextField
+          select
+          label="Názov"
+          value={item.columnName}
+          name={item.accessor}
+          onChange={(e) => handleColumnNameChange(e, 'columnName', e.target.value)}
+          className="w-[165px]"
+        >
+          {columnOptions.map((option) => (
+            <MenuItem key={option.name} value={option.name}>
+              {option.name}
+            </MenuItem>
+          ))}
+        </TextField>
+        <TextField
+          type="text"
+          label="Vysvetlivka"
+          value={item.desc}
+          name={item.accessor}
+          onChange={(e) => handleChange(e, 'desc', e.target.value)}
+          className="w-[255px]"
+        />
+        <TextField
+          type="text"
+          label="Jednotka"
+          value={item.unit}
+          name={item.accessor}
+          onChange={(e) => handleChange(e, 'unit', e.target.value)}
+          className="w-[80px] px-6"
+        />
+        <TextField
+          type="number"
+          label="Min."
+          value={item.min || undefined}
+          name={item.accessor}
+          onChange={(e) => handleChange(e, 'min', Number(e.target.value))}
+          className="w-[70px] pr-6"
+        />
+        <TextField
+          type="number"
+          label="Max."
+          value={item.max || undefined}
+          name={item.accessor}
+          onChange={(e) => handleChange(e, 'max', Number(e.target.value))}
+          className="w-[70px]"
+        />
+        <Switch
+          checked={!item.hide}
+          name={item.accessor}
+          onChange={(e) => handleChange(e, 'hide', !e.target.checked)}
+        />
+      </ListItem>
+    );
+  }
+);
 
 export default SettingsModalColumn;
