e0cda88747b44f3cc2e9ec8a000f2b7b533ab4bd
Vladimír Tomko <82036512+VladimirTomko@users.noreply.github.com>
2024-03-13 17:17:12 +0100

    be endpoint

--- src/app/config/axiosConfig.ts
+++ src/app/config/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5500', //'https://api.monitoringpro.sk',
+  baseURL: 'https://api.monitoringpro.sk',
   headers: {
     'mp-auth': localStorage.getItem('monitoringpro-auth') || '',
   },
