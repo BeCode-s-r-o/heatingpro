54c1dff02e08eb43f12927189fe08e06f47ac90b
PeterZajac <zajacp011@gmail.com>
2024-03-18 19:13:59 +0100

    Update axiosConfig.ts to use environment variable for isDev

--- src/app/config/axiosConfig.ts
+++ src/app/config/axiosConfig.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const isDev = true;
+const isDev = process.env.NODE_ENV === 'development';
 
 const axiosInstance = axios.create({
   baseURL: isDev ? 'http://localhost:5500/' : 'https://api.monitoringpro.sk/',
